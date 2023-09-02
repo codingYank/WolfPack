6891e76367b497f63ce6cbfc230456379f9b5009
codingYank <tyler.schmidt99@gmail.com>
2023-09-02 16:45:58 -0400

    mongoose models

---
+++ backend/models/post.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose"
+
+const postSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    quoting: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+    comments: [postSchema],
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Post = mongoose.model("Post", postSchema)
+
+export default Post
---
+++ backend/models/user.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    handle: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      required: true,
+    },
+    following: [userSchema],
+    foloowers: [userSchema],
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const User = mongoose.model("User", userSchema)
+
+export default User
