62497578dd73a149154d48282f77a7a85fa70b0f
codingYank <tyler.schmidt99@gmail.com>
2023-09-10 21:08:05 -0400

    user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js"
 import User from "../models/user.js"
-import jwt from "jsonwebtoken"
+import generateToken from "../utils/generateToken.js"
 
 //@desc Auth user & get token
 //@route POST /api/users/login
@@ -11,17 +11,7 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email })
 
   if (user && (await user.matchPassword(password))) {
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "30d",
-    })
-
-    // set JWT as HTTP-Only cookie
-    res.cookie("jwt", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== "development",
-      sameSite: "strict",
-      maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
-    })
+    generateToken(res, user._id)
 
     res.json({
       _id: user._id,
@@ -41,14 +31,56 @@ const authUser = asyncHandler(async (req, res) => {
 //@route POST /api/users/
 //@access Public
 const registerUser = asyncHandler(async (req, res) => {
-  res.send("register user")
+  const { email, password, handle, name, profilePicture, description } =
+    req.body
+
+  const userExists = await User.findOne({ email })
+  const handleTaken = await User.findOne({ handle })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error("Email already used")
+  } else if (handleTaken) {
+    res.status(400)
+    throw new Error("Handle not available")
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    handle,
+    profilePicture,
+    description,
+  })
+
+  if (user) {
+    generateToken(res, user._id)
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      handle: user.handle,
+      profilePicture: user.profilePicture,
+      description: user.description,
+    })
+  } else {
+    res.status(400)
+    throw new Error("Invalid user data")
+  }
 })
 
 //@desc logout user & destroy token
 //@route POST /api/users/logout
 //@access Private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("logout user")
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  })
+
+  res.status(200).json({ message: "Logged out successfully" })
 })
 
 //@desc Get user profile
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,12 +7,16 @@ import {
   registerUser,
   updateUserProfile,
 } from "../controllers/userController.js"
+import { protect } from "../middleware/authMiddleware.js"
 const router = express.Router()
 
 router.route("/").post(registerUser)
 router.post("/logout", logoutUser)
 router.post("/login", authUser)
-router.route("/profile").get(getUserProfile).put(updateUserProfile)
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile)
 router.route("/:id").get(getUserByID)
 
 export default router
