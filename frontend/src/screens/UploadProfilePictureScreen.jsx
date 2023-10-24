167cfb7870b91778c41817d4455e529596bd5a0e
codingYank <tyler.schmidt99@gmail.com>
2023-10-24 16:16:10 -0400

    upload profile pic styles

--- frontend/src/screens/UploadProfilePictureScreen.jsx
+++ frontend/src/screens/UploadProfilePictureScreen.jsx
@@ -4,6 +4,7 @@ import { toast } from 'react-toastify'
 import { Accent3Button } from '../assets/components/button'
 import { useDispatch } from 'react-redux'
 import { setCredentials } from '../slices/authSlice'
+import '../assets/styles/button.css'
 
 const UploadProfilePictureScreen = ({show, setShow, refetch}) => {
 
@@ -37,9 +38,18 @@ const UploadProfilePictureScreen = ({show, setShow, refetch}) => {
     return (
       <div className='modal-screen'>
         <div className='modal-content'>
-          <form>
-            <label htmlFor="image">Choose Profile Picture</label>
-            <input type='file' id='image' onChange={uploadFileHandler}></input>
+          <h3 style={{
+            marginTop: 0
+          }}>Upload Profile Picture</h3>
+          <form style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '30px',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+            <label htmlFor="image" className='accent1btn'>Choose Profile Picture</label>
+            <input type='file' id='image' onChange={uploadFileHandler} accept='.jpeg, .jpg, .png, .webp' style={{position: 'absolute', left: '-99999999999px'}}></input>
             <Accent3Button onClick={onClose}>Close</Accent3Button>
           </form>
         </div>
