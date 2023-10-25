aea9be3d5ee49ddbf5d14acc77e59357481adef9
codingYank <tyler.schmidt99@gmail.com>
2023-10-25 19:11:23 -0400

    uploads

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, "/uploads")
+    cb(null, "/project/src/uploads")
   },
   filename(req, file, cb) {
     cb(
